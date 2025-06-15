f2a75b8a14e4608bee0f2d4d2cb8d68630dc4aec
Diehard <wendyrzechula@gmail.com>
2025-06-14 23:48:32 -0500

    “week-2”

---
+++ week-2/quote_printer.py
@@ -0,0 +1,10 @@
+"""
+Author: Wendy Rzechula
+Date: 6/14/2025
+File Name: quote_printer.py
+Description: A simple Python program that prints a quote and the authors name.
+"""
+
+# Prints a quote and author's name
+print("'Life isn't about waiting for the storm to pass, it's about learning to dance in the rain '")
+print("- Vivian Greene")
