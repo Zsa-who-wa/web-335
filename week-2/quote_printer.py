8931769825b768c8ba8266cc3ba93794a821bb93
Diehard <wendyrzechula@gmail.com>
2025-06-19 14:49:37 -0500

    Updated quote_printer.py and removed unneeded files

--- week-2/quote_printer.py
+++ week-2/quote_printer.py
@@ -3,6 +3,7 @@ Author: Wendy Rzechula
 Date: 6/14/2025
 File Name: quote_printer.py
 Description: A simple Python program that prints a quote and the authors name.
+Github Link: https://github.com/Zsa-who-wa/web-335.git
 """
 
 # Prints a quote and author's name
